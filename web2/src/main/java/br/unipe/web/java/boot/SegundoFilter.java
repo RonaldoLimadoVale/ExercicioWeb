447c2e87fe3ca9263d300f0ddc13bb15261c63bf
RonaldoLimadoVale <47544489+RonaldoLimadoVale@users.noreply.github.com>
2019-10-26 18:04:42 -0300

    Add files via upload

---
+++ web2/src/main/java/br/unipe/web/java/boot/SegundoFilter.java
@@ -0,0 +1,43 @@
+package br.unipe.web.java.boot;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+public class SegundoFilter {
+
+	@WebFilter("/p")
+	public class NomeFilter implements Filter {
+		public void init(FilterConfig filterConfig) throws ServletException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+				throws IOException, ServletException {
+			System.out.println("Passando segundo filtro");
+		
+			String nome = request.getParameter("nome");		
+			if(nome==null) {
+				request.setAttribute("nome", "Pedro");
+				
+			}
+			chain.doFilter(request, response);
+			
+		}
+		
+		public void destroy() {
+			// TODO Auto-generated method stub
+			
+		}
+
+
+	}
+
+}
---
+++ web2/src/main/java/br/unipe/web/java/boot/SegurancaFilter.java
@@ -0,0 +1,41 @@
+package br.unipe.web.java.boot;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/fujioka")
+public class SegurancaFilter implements Filter {
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		System.out.println("Passando pelo Filtro 1");
+	
+		String nome = request.getParameter("nome");		
+		if(nome==null) {
+			request.setAttribute("nome", "eu");
+			
+		}
+		chain.doFilter(request, response);
+		
+	}
+	
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+  
+
+}
